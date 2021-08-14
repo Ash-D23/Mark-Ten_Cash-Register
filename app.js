14c3933b25e0d580476d28c26ae6f7f7bef605e9
Ash-D23 <ak6214651@gmail.com>
2021-08-14 19:55:23 +0530

    cash register

---
+++ app.js
@@ -0,0 +1,47 @@
+var bill = document.getElementById("bill")
+var cash = document.getElementById("cash")
+
+var btn = document.getElementById("mybtn")
+
+var output = document.querySelector(".output")
+
+btn.addEventListener("click", calculatenotes)
+
+function calculatenotes(){
+  var notes=[2000,500,100,20,10,5,1]
+  var values=[]
+  var current=Number(cash.value)
+  var amount=Number(bill.value)
+  if (current <= amount){
+    alert("Cash is less or equal to bill")
+
+  }else{
+    current=current-amount
+
+    for(var i=0; i<notes.length; i++){
+      if (current>=notes[i]){
+
+        values.push(Math.trunc(current/notes[i]))
+        current=current%notes[i]
+      }
+      else{
+        values.push(0)
+      }
+    }
+
+  }
+  output.classList.remove("hide");
+  displayoutput(values)
+
+}
+
+function displayoutput(values){
+  //var element = document.getElementById("myDIV");
+  //element.classList.add("mystyle")
+  console.log(values)
+  var rows = document.querySelectorAll(".table-values")
+
+  for(var i=0; i<rows.length;i++){
+    rows[i].innerText = values[i]
+  }
+}
